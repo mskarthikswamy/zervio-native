66e908748af0646b287e13765046eb2bb00cacf2
mskarthikswamy <karthik.swamy@makoitlab.com>
2023-06-05 17:14:40 +0530

    feat/ui:folder structure

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>zervio native</Text>
       <StatusBar style="auto" />
     </View>
   );
